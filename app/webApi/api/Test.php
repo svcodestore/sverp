c4ee76ed2f3bc58772daefb6b336b4882e7bda46
Your Name <you@example.com>
2021-05-14 16:58:06 +0800

    测试

--- app/webApi/api/Test.php
+++ app/webApi/api/Test.php
@@ -469,4 +469,215 @@ English / 正體中文 123 Chinese 测试 测试测
         dump($odbc);
         odbc_close($odbc);
     }
+
+    public function dbfreader()
+    {
+        $fmt = function ($str) {
+            $s = str_replace(' ', '', @iconv('GBK', 'UTF-8', $str));
+            if (ord(substr($s, -1)) < 10) {
+                return substr($s, 0, -1);
+            }
+
+            return  $s;
+        };
+
+        $path = '//192.168.123.252/data$/database/';
+        $Test = new Prodigy_DBF($path . 'employee.DBF', $path . 'employee.FPT');
+        $info = [];
+        while (($Record = $Test->GetNextRecord(true))) {
+
+            $row['positionRankCode'] = $Record['NBJB'];
+            $row['name'] = $fmt($Record['NAME']) === false ? '' : $fmt($Record['NAME']);
+            $row['dept'] =  $fmt($Record['DEPTS']) === false ? '' : $fmt($Record['DEPTS']);
+            $row['grp'] = $fmt($Record['GRP']) === false ? '' : $fmt($Record['GRP']);
+            $row['subGrp'] = $fmt($Record['GRPS']) === false ? '' : $fmt($Record['GRPS']);
+            $row['position'] = $fmt($Record['POSITIONS']) === false ? '' : $fmt($Record['POSITIONS']);
+            $row['isLeaveJob'] = $fmt($Record['RETIRETYPE']) === false ? '' : $fmt($Record['RETIRETYPE']);
+            $row['joinDate'] = $fmt($Record['ENTERDATE']) === false ? '' : $fmt($Record['ENTERDATE']);
+            $row['staffNo'] = $fmt($Record['WORKNO']) === false ? '' : $fmt($Record['WORKNO']);
+            // $row['born'] = $fmt($Record['BORN']);
+            // $row['education'] = $fmt($Record['DEGREE']);
+
+            // foreach (array_keys($Record) as $key => $value) {
+            //     print_r($value);
+            //     print_r(' ===> ');
+            //     print_r( $fmt($Record[$value]));
+            //     print_r('<br>');
+            // }
+            // print_r('<br>');
+
+            if ($row['staffNo'] > 99 && $row['isLeaveJob'] === '在职') {
+
+                $info[] = $row;
+            }
+        }
+
+        return json($info);
+    }
+}
+
+
+
+
+
+class Prodigy_DBF
+{
+    private $Filename, $DB_Type, $DB_Update, $DB_Records, $DB_FirstData, $DB_RecordLength, $DB_Flags, $DB_CodePageMark, $DB_Fields, $FileHandle, $FileOpened;
+    private $Memo_Handle, $Memo_Opened, $Memo_BlockSize;
+
+    private function Initialize()
+    {
+
+        if ($this->FileOpened) {
+            fclose($this->FileHandle);
+        }
+
+        if ($this->Memo_Opened) {
+            fclose($this->Memo_Handle);
+        }
+
+        $this->FileOpened = false;
+        $this->FileHandle = NULL;
+        $this->Filename = NULL;
+        $this->DB_Type = NULL;
+        $this->DB_Update = NULL;
+        $this->DB_Records = NULL;
+        $this->DB_FirstData = NULL;
+        $this->DB_RecordLength = NULL;
+        $this->DB_CodePageMark = NULL;
+        $this->DB_Flags = NULL;
+        $this->DB_Fields = array();
+
+        $this->Memo_Handle = NULL;
+        $this->Memo_Opened = false;
+        $this->Memo_BlockSize = NULL;
+    }
+
+    public function __construct($Filename, $MemoFilename = NULL)
+    {
+        $this->Prodigy_DBF($Filename, $MemoFilename);
+    }
+
+    public function Prodigy_DBF($Filename, $MemoFilename = NULL)
+    {
+        $this->Initialize();
+        $this->OpenDatabase($Filename, $MemoFilename);
+    }
+
+    public function OpenDatabase($Filename, $MemoFilename = NULL)
+    {
+        $Return = false;
+        $this->Initialize();
+
+        $this->FileHandle = fopen($Filename, "r");
+        if ($this->FileHandle) {
+            // DB Open, reading headers
+            $this->DB_Type = dechex(ord(fread($this->FileHandle, 1)));
+            $LUPD = fread($this->FileHandle, 3);
+            $this->DB_Update = ord($LUPD[0]) . "/" . ord($LUPD[1]) . "/" . ord($LUPD[2]);
+            $Rec = unpack("V", fread($this->FileHandle, 4));
+            $this->DB_Records = $Rec[1];
+            $Pos = fread($this->FileHandle, 2);
+            $this->DB_FirstData = (ord($Pos[0]) + ord($Pos[1]) * 256);
+            $Len = fread($this->FileHandle, 2);
+            $this->DB_RecordLength = (ord($Len[0]) + ord($Len[1]) * 256);
+            fseek($this->FileHandle, 28); // Ignoring "reserved" bytes, jumping to table flags
+            $this->DB_Flags = dechex(ord(fread($this->FileHandle, 1)));
+            $this->DB_CodePageMark = ord(fread($this->FileHandle, 1));
+            fseek($this->FileHandle, 2, SEEK_CUR);    // Ignoring next 2 "reserved" bytes
+
+            // Now reading field captions and attributes
+            while (!feof($this->FileHandle)) {
+
+                // Checking for end of header
+                if (ord(fread($this->FileHandle, 1)) == 13) {
+                    break;  // End of header!
+                } else {
+                    // Go back
+                    fseek($this->FileHandle, -1, SEEK_CUR);
+                }
+
+                $Field["Name"] = trim(fread($this->FileHandle, 11));
+                $Field["Type"] = fread($this->FileHandle, 1);
+                fseek($this->FileHandle, 4, SEEK_CUR);  // Skipping attribute "displacement"
+                $Field["Size"] = ord(fread($this->FileHandle, 1));
+                fseek($this->FileHandle, 15, SEEK_CUR); // Skipping any remaining attributes
+                $this->DB_Fields[] = $Field;
+            }
+
+            // Setting file pointer to the first record
+            fseek($this->FileHandle, $this->DB_FirstData);
+
+            $this->FileOpened = true;
+
+            // Open memo file, if exists
+            if (!empty($MemoFilename) and file_exists($MemoFilename) and preg_match("%^(.+).fpt$%i", $MemoFilename)) {
+                $this->Memo_Handle = fopen($MemoFilename, "r");
+                if ($this->Memo_Handle) {
+                    $this->Memo_Opened = true;
+
+                    // Getting block size
+                    fseek($this->Memo_Handle, 6);
+                    $Data = unpack("n", fread($this->Memo_Handle, 2));
+                    $this->Memo_BlockSize = $Data[1];
+                }
+            }
+        }
+
+        return $Return;
+    }
+
+    public function GetNextRecord($FieldCaptions = false)
+    {
+        $Return = NULL;
+        $Record = array();
+
+        if (!$this->FileOpened) {
+            $Return = false;
+        } elseif (feof($this->FileHandle)) {
+            $Return = NULL;
+        } else {
+            // File open and not EOF
+            fseek($this->FileHandle, 1, SEEK_CUR);  // Ignoring DELETE flag
+            foreach ($this->DB_Fields as $Field) {
+                $RawData = fread($this->FileHandle, $Field["Size"]);
+                // Checking for memo reference
+                if ($Field["Type"] == "M" and $Field["Size"] == 4 and !empty($RawData)) {
+                    // Binary Memo reference
+                    $Memo_BO = unpack("V", $RawData);
+                    if ($this->Memo_Opened and $Memo_BO != 0) {
+                        fseek($this->Memo_Handle, $Memo_BO[1] * $this->Memo_BlockSize);
+                        $Type = unpack("N", fread($this->Memo_Handle, 4));
+                        if ($Type[1] == "1") {
+                            $Len = unpack("N", fread($this->Memo_Handle, 4));
+                            $Value = trim(fread($this->Memo_Handle, $Len[1]));
+                        } else {
+                            // Pictures will not be shown
+                            $Value = "{BINARY_PICTURE}";
+                        }
+                    } else {
+                        $Value = "{NO_MEMO_FILE_OPEN}";
+                    }
+                } else {
+                    $Value = trim($RawData);
+                }
+
+                if ($FieldCaptions) {
+                    $Record[$Field["Name"]] = $Value;
+                } else {
+                    $Record[] = $Value;
+                }
+            }
+
+            $Return = $Record;
+        }
+
+        return $Return;
+    }
+
+    function __destruct()
+    {
+        // Cleanly close any open files before destruction
+        $this->Initialize();
+    }
 }
