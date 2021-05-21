e9bbc26eebe4e4cddf27af97a51e2eef903a2cce
yangwb1123 <1832227346@qq.com>
2021-05-21 15:58:38 +0800

    更新

--- app/webApi/api/Prod.php
+++ app/webApi/api/Prod.php
@@ -2,7 +2,7 @@
 /*
  * @Author: yanbuw1911
  * @Date: 2020-11-18 15:07:48
- * @LastEditTime: 2021-03-18 14:57:50
+ * @LastEditTime: 2021-05-21 15:58:21
  * @LastEditors: yanbuw1911
  * @Description: 
  * @FilePath: /sverp/app/webApi/api/Prod.php
@@ -13,7 +13,7 @@ namespace app\webApi\api;
 use app\BaseController;
 use app\webApi\controller\Prod as ControllerProd;
 
-class Prod
+class Prod extends BaseController
 {
     public function getCalenderData()
     {
