4931ba56a467dd649c91794687f9b1a90be405d8
mokch <buxueys@gmail.com>
2021-07-20 10:15:50 +0800

    remove some info

--- app/command/TPMCopyAliyunData.php
+++ app/command/TPMCopyAliyunData.php
@@ -2,10 +2,11 @@
 /*
  * @Date: 2021-05-18 15:42:38
  * @LastEditors: Mok.CH
- * @LastEditTime: 2021-05-18 16:11:01
- * @FilePath: \sverp\app\command\TPMCopyAliyunData.php
+ * @LastEditTime: 2021-07-20 10:12:56
+ * @FilePath: /sverp/app/command/TPMCopyAliyunData.php
  */
-declare (strict_types = 1);
+
+declare(strict_types=1);
 
 namespace app\command;
 
@@ -27,26 +28,30 @@ class TPMCopyAliyunData extends Command
             ->setDescription('定时获取aliyun上外网保存的报修记录到内网服务器');
     }
 
-    
+
     protected function execute(Input $input, Output $output)
     {
         // 指令输出
         // $output->writeln('tpmcopyaliyundata');
-        try{
-            $db = new PDO('mysql:host=47.112.192.40;dbname=starvc_homedb', 'root', 'root');
+        try {
+            $host = env('ALIYUN_DB_HOST');
+            $dbname = env('ALIYUN_DB_NAME');
+            $dbuser = env('ALIYUN_DB_USERNAME');
+            $dbpasswd = env('ALIYUN_DB_PASSWD');
+            $db = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $dbuser, $dbpasswd);
         } catch (Exception $e) {
             echo $e->getMessage();
             return;
         }
-        
-        
+
+
         $runtime_path = app()->getRuntimePath();
         $last_id_record_file = join('\\', [$runtime_path, 'aliyun_tpm_last_id.txt']);
         if (!is_file($last_id_record_file)) {
             touch($last_id_record_file);
         }
         $last_id = intval(file_get_contents($last_id_record_file));
-        
+
         $SQL = "SELECT * FROM prodlib_repair_record WHERE id > {$last_id}";
 
         $data = $db->query($SQL)->fetchAll(PDO::FETCH_ASSOC);
@@ -60,7 +65,7 @@ class TPMCopyAliyunData extends Command
                 $record_model->addRecord($row);
             }
         }
-        
+
         file_put_contents($last_id_record_file, $last_id);
     }
 }
